e7368fb85323e851743415e6794d2f9dc7581e4c
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-09-19 23:15:06 +0530

    index.ts file store

--- components/data/index.ts
+++ components/data/index.ts
@@ -73,28 +73,28 @@ export const navItems = [
         "From concept to launch, Nazish Ahmed was professional, creative, and incredibly efficient. They turned our vision into reality, and the results speak for themselves.",
       name: "Shoeb Ahmed",
       title: "Senior Digital Marketing Specialist (Redimpact)",
-      image: "https://media.discordapp.net/attachments/1257783340673536053/1259766938012352542/abbu-pfp.jpeg?ex=66e140e3&is=66dfef63&hm=0abe6b816ea67ff8e3bc03102cc1803bfc0016a200576d506f2c54647b8250ab&=&format=webp&width=350&height=350"
+      image: "https://images.pexels.com/photos/5009198/pexels-photo-5009198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
       quote:
         "I cant praise Nazish Ahmed enough for their amazing work. They not only created a beautiful website but also ensured it was optimized for performance and SEO. Our online presence has never been stronger.",
       name: "Habeeb Ahmed Khan",
       title: "Senior Lead Manager - MEP & Sustainability",
-      image: "https://media.discordapp.net/attachments/1257783340673536053/1259766937739726899/ansar-chacha.jpeg?ex=66e140e3&is=66dfef63&hm=3b6978cde038106e40f96853426dc561f61f45e7cf408c2632b6ed200b2df3d6&=&format=webp&width=662&height=662"
+      image: "https://images.pexels.com/photos/5009198/pexels-photo-5009198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
       quote:
         "Exceptional service and top-notch results! Nazish Ahmed took our outdated website and revamped it into a sleek, functional masterpiece. Our customers are impressed, and so are we.",
       name: "Mohammed Fawaaz Moin",
       title: "Mechanical Engineer | LEED® Green Associate™",
-      image: "https://media.discordapp.net/attachments/1257783340673536053/1259766937362366557/fawaaz-bhaiya.jpeg?ex=66e140e3&is=66dfef63&hm=11b3adf2f9f1fcd4076fa592f9b73933b693dab336dcd00393497157f9743878&=&format=webp&width=662&height=662"
+      image: "https://images.pexels.com/photos/5009198/pexels-photo-5009198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
       quote:
         "Collaborating with Nazish was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nazish's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nazish is the ideal partner.",
       name: "Mohammed Maaz Waheed",
       title: "MERN Stack Developer at Master Works",
-      image: "https://media.discordapp.net/attachments/1257783340673536053/1259766938284855296/maaz-bhaiya.jpeg?ex=66e140e3&is=66dfef63&hm=c6a06be9e326fa696f1276752a4377c9f038bbfa162fc644ec2eb48ca1d697da&=&format=webp&width=662&height=662"
+      image: "https://images.pexels.com/photos/5009198/pexels-photo-5009198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
   ];
   export const workExperience = [
@@ -107,14 +107,14 @@ export const navItems = [
     },
     {
       id: 2,
-      title: "Frontend Developer",
+      title: "Web Developer",
       desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
       className: "md:col-span-2",
       thumbnail: "https://oratorbot.xyz/img/logo-nobg.png",
     },
     {
       id: 3,
-      title: "Freelancer",
+      title: "Full stack Developer",
       desc: "Freelance web developer creating high-performing, user-friendly websites that exceed client expectations.",
       className: "md:col-span-2",
       thumbnail: "https://img.icons8.com/?size=100&id=ngc6JsBomclm&format=png&color=000000",
